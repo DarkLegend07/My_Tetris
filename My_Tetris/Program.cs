820de375fe671d1d09354fd719feadd8d428e205
DarkLegend07 <83396872+DarkLegend07@users.noreply.github.com>
2021-09-26 00:17:58 +0500

    Update Program.cs
    Добавил !!

--- My_Tetris/Program.cs
+++ My_Tetris/Program.cs
@@ -6,7 +6,7 @@ namespace My_Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
